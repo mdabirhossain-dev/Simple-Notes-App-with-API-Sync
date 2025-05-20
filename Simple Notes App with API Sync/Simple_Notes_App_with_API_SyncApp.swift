aa49b4b379f58c17d60506644a810a86dbb1e127
Md Abir Hossain <hossain.mdabir@gmail.com>
2025-05-20 14:21:30 +0600

    Initial Commit

---
+++ Simple Notes App with API Sync/Simple_Notes_App_with_API_SyncApp.swift
@@ -0,0 +1,23 @@
+//
+// 
+// FileName: Simple_Notes_App_with_API_SyncApp.swift
+// ProjectName: Simple Notes App with API Sync
+//
+// Created by MD ABIR HOSSAIN on 20-05-2025 at 2:21 PM
+// Website: https://mdabirhossain.com/
+//
+
+
+import SwiftUI
+
+@main
+struct Simple_Notes_App_with_API_SyncApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
